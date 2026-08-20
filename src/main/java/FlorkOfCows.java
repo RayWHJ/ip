2d7a8830323d84091c536ad7d750317eb6ab0f82
Wong Hau Jet <146151820+RayWHJ@users.noreply.github.com>
2026-08-20 18:06:23 +0800

    Updated FlorkOfCows

--- src/main/java/FlorkOfCows.java
+++ src/main/java/FlorkOfCows.java
@@ -1,12 +1,21 @@
 public class FlorkOfCows {
     public static void main(String[] args) {
-        String banner = "  ______ _            _     ____   __  _____                  \n" +
-                " |  ____| |          | |   / __ \\ / _|/ ____|                 \n" +
-                " | |__  | | ___  _ __| | _| |  | | |_| |     _____      _____ \n" +
-                " |  __| | |/ _ \\| '__| |/ / |  | |  _| |    / _ \\ \\ /\\ / / __|\n" +
-                " | |    | | (_) | |  |   <| |__| | | | |___| (_) \\ V  V /\\__ \\\n" +
-                " |_|    |_|\\___/|_|  |_|\\_\\\\____/|_|  \\_____\\___/ \\_/\\_/ |___/\n";
+        String banner = "  ______ _            _     ____   __  _____                  \n"
+                + " |  ____| |          | |   / __ \\ / _|/ ____|                 \n"
+                + " | |__  | | ___  _ __| | _| |  | | |_| |     _____      _____ \n"
+                + " |  __| | |/ _ \\| '__| |/ / |  | |  _| |    / _ \\ \\ /\\ / / __|\n"
+                + " | |    | | (_) | |  |   <| |__| | | | |___| (_) \\ V  V /\\__ \\\n"
+                + " |_|    |_|\\___/|_|  |_|\\_\\\\____/|_|  \\_____\\___/ \\_/\\_/ |___/\n";
 
+        // Print banner and simple greeting interaction per requirement
+        System.out.println("____________________________________________________________");
         System.out.println(banner);
+        System.out.println("Hello! I'm FlorkOfCows.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
